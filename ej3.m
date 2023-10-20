b2664e88e82d8ae78590fc2792420bd02fa5d6e0
fran855 <65423343+fran855@users.noreply.github.com>
2023-10-20 11:12:47 -0300

    La reconstrucción no funciona

--- ej3.m
+++ ej3.m
@@ -27,3 +27,31 @@ end
 % x(n) = \sum_{k=1}^P a_k x(n-k) + G u(n)
 % Como el audio es 'a' -> tren de impulsos periódicos u(n) = sum_k
 % delta(n-k N0) con f = 200 Hz
+f0 = 200;
+
+t = 0:1/fs:0.03-1/fs; % Tiempo de 0.03 segundo
+u = square(2*pi*f0*t); % Tren de pulsos
+
+segmentos = [];
+for i = 1 : length(coeficientes)
+    % Inicializo el segmento reconstruido x(n)
+    x = zeros(1, length(u));
+
+    % Recorre el tiempo n para la reconstrucción
+    for n = 1:length(u) % Comenzar en 1
+        for k = 1:P
+            if n - k > 0
+                x(n) = x(n) + coeficientes(k, i) * x(n - k);
+            end
+        end
+        x(n) = x(n) + G * u(n);
+    end
+
+    segmentos = [segmentos x];
+end
+
+reconstruccion = [];
+for i = 1 : length(segmentos)
+    reconstruccion = [reconstruccion segmentos(i)];
+end
+
